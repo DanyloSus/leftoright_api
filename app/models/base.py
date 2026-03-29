295bc489b6874019944936e4f3f5d5b2e71c4a07
Danylo Sushko <dsu@empat.tech>
2026-03-29 22:36:40 +0300

    feat(models): add TimestampMixin for automatic created_at and updated_at fields

--- app/models/base.py
+++ app/models/base.py
@@ -1,5 +1,15 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Model(DeclarativeBase):
     pass
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), server_default=func.now(), onupdate=func.now()
+    )
