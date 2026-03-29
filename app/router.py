d7fa76a55b16fc618b300efa7fe04fba3475caff
Danylo Sushko <dsu@empat.tech>
2026-03-29 22:38:53 +0300

    feat(router): reorganize router imports and include authentication routes

--- app/router.py
+++ app/router.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+
+from app.features.auth import router as auth_router
 from app.features.user import api
-                                   
-api_router = APIRouter()                                      
-api_router.include_router(api.router, prefix="/users", tags=["Users"])    
+
+api_router = APIRouter()
+api_router.include_router(auth_router, prefix='/auth', tags=['Authentication'])
+api_router.include_router(api.router, prefix='/users', tags=['Users'])
