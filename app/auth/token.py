2fd1a79205b2a6ff74c492cf66c6fd8f273d20c1
Danylo Sushko <dsu@empat.tech>
2026-03-29 22:24:17 +0300

    feat(token): implement JWT token creation and verification functions

---
+++ app/auth/token.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
+
+from app.exceptions import ErrPermissionDenied
+from app.result import Err, Ok
+
+
+def create_jwt_token(secret_key: str, algorithm: str, data: dict, expires: timedelta) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires
+    to_encode['exp'] = expire
+    return jwt.encode(to_encode, secret_key, algorithm=algorithm)
+
+
+def verify_token(token: str, secret_key: str, algorithm: str):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=[algorithm])
+        return Ok(payload)
+    except jwt.ExpiredSignatureError:
+        return Err(ErrPermissionDenied('Token expired'))
+    except jwt.InvalidTokenError:
+        return Err(ErrPermissionDenied('Invalid token'))
