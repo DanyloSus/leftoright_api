962d84fdebe40b05a1c079c1cba0ce8a1a524492
Danylo Sushko <dsu@empat.tech>
2026-03-29 22:06:41 +0300

    feat(session): add session dependency injection for async database sessions

--- configs/session.py
+++ configs/session.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from .database import db_settings
@@ -10,3 +13,6 @@ AsyncSessionFactory = async_sessionmaker(engine, expire_on_commit=False)
 async def get_session() -> AsyncSession:  # type: ignore[return]
     async with AsyncSessionFactory() as session:
         yield session
+
+
+AsyncSessionDep = Annotated[AsyncSession, Depends(get_session)]
