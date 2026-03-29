3f5df639bf043eb21a86582d207c921b98d557d2
Danylo Sushko <dsu@empat.tech>
2026-03-29 22:49:59 +0300

    feat(auth): implement user ID retrieval with token verification

---
+++ app/di/auth.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.exceptions import HTTPException
+from fastapi.security.http import HTTPAuthorizationCredentials
+
+from app.auth.http import HTTPBearer
+from app.auth.token import verify_token
+from configs.jwt import JWTConfigDep
+
+bearer_scheme = HTTPBearer()
+
+
+def _get_user_id(
+    config: JWTConfigDep,
+    token: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+) -> int:
+    data, err = verify_token(
+        token=token.credentials,
+        secret_key=config.SECRET_KEY,
+        algorithm=config.ALGORITHM,
+    )
+    if err:
+        raise HTTPException(status_code=401, detail='Invalid token')
+
+    user_id = data.get('user_id')
+    if user_id is None:
+        raise HTTPException(status_code=401, detail='Invalid token')
+
+    return user_id
+
+
+UserIdDep = Annotated[int, Depends(_get_user_id)]
