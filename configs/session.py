1236994318d83ff7ffe9bccae78a0660c8b0abaa
Danylo Sushko <dsu@empat.tech>
2026-03-29 20:41:10 +0300

    feat(session): add async session management with SQLAlchemy

---
+++ configs/session.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from .database import db_settings
+
+engine = create_async_engine(db_settings.async_url)
+
+AsyncSessionFactory = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session() -> AsyncSession:  # type: ignore[return]
+    async with AsyncSessionFactory() as session:
+        yield session
