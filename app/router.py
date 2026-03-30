dbbec1532138cd029422aa55b04c138355a5f40a
Danylo Sushko <dsu@empat.tech>
2026-03-30 19:30:18 +0300

    feat(router): add tournaments API router to main API router

--- app/router.py
+++ app/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from app.features.auth import router as auth_router
+from app.features.tournament import api as tournament_api
 from app.features.user import api
 
 api_router = APIRouter()
 api_router.include_router(auth_router, prefix='/auth', tags=['Authentication'])
 api_router.include_router(api.router, prefix='/users', tags=['Users'])
+api_router.include_router(tournament_api.router, prefix='/tournaments', tags=['Tournaments'])
