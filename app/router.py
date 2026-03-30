0732fe0b627a1709cbebed02d7371098128d4808
Danylo Sushko <dsu@empat.tech>
2026-03-30 21:44:19 +0300

    feat(router): include entity and session routers in API routes

--- app/router.py
+++ app/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.features.auth import router as auth_router
+from app.features.entity import api as entity_api
+from app.features.session import api as session_api
 from app.features.tournament import api as tournament_api
 from app.features.user import api
 
@@ -8,3 +10,6 @@ api_router = APIRouter()
 api_router.include_router(auth_router, prefix='/auth', tags=['Authentication'])
 api_router.include_router(api.router, prefix='/users', tags=['Users'])
 api_router.include_router(tournament_api.router, prefix='/tournaments', tags=['Tournaments'])
+api_router.include_router(entity_api.router, prefix='/tournaments/{tournament_id}/entities', tags=['Entities'])
+api_router.include_router(session_api.tournament_router, prefix='/tournaments/{tournament_id}/sessions', tags=['Sessions'])
+api_router.include_router(session_api.session_router, prefix='/sessions', tags=['Sessions'])
